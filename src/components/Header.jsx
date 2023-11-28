b2088c3d385b99161801314af21fd664114d9cf3
gfuselier <gisellefuselier@gmail.com>
2023-11-27 18:23:07 -0600

    added header div and h1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,21 +4,24 @@ import Navigation from './Navigation';
 export default function Header() {
   // The Nav UI component will render each of the Link elements in the links prop
   return (
-    <Navigation
-      links={[
-        <Link key={1} className="nav-link text-light" to="/">
-          About Me
-        </Link>,
-        <Link key={2} className="nav-link text-light" to="/portfolio">
-          Portfolio
-        </Link>,
-        <Link key={3} className="nav-link text-light" to="/contact">
-          Contact
-        </Link>,
-        <Link key={4} className="nav-link text-light" to="/resume">
-          Resume
-        </Link>
-      ]}
-    />
+    <header className="header">
+      <h1>Giselle Fuselier</h1>
+      <Navigation
+        links={[
+          <Link key={1} className="nav-link text-light" to="/">
+            About Me
+          </Link>,
+          <Link key={2} className="nav-link text-light" to="/portfolio">
+            Portfolio
+          </Link>,
+          <Link key={3} className="nav-link text-light" to="/contact">
+            Contact
+          </Link>,
+          <Link key={4} className="nav-link text-light" to="/resume">
+            Resume
+          </Link>
+        ]}
+      />
+    </header>
   );
 }
