83d70398a75044c000150513e71fede0e82df5a6
gfuselier <gisellefuselier@gmail.com>
2023-11-27 18:49:45 -0600

    added images

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,18 +1,46 @@
-import Card from "./Card"
 
 const projects = [
     {
         id: 1,
         title: "Weather Dashboard",
         href: "https://gfuselier.github.io/weather-dashboard",
-        
-    }
+        languages: "HTML, CSS, JavaScript",
+        image: ""
+    },
+    {
+        id: 2,
+        title: "Movie Grabber",
+        href: "https://gfuselier.github.io/movie-grabber",
+        languages: "HTML, CSS, JavaScript, Bulma",
+        image: ""
+      }, {
+        id: 3,
+        title: "Work-Day Scheduler",
+        href: "https://gfuselier.github.io/work-day-scheduler",
+        languages: "HTML, CSS, JavaScript",
+        image: ""
+      }, {
+        id: 4,
+        title: "Password Generator",
+        href: "https://gfuselier.github.io/password-generator",
+        languages: "HTML, CSS, JavaScript",
+        image: ""
+      }, {
+        id: 5,
+        title: "Horiseon Accessibility Refactor",
+        href: "https://gfuselier.github.io/password-generator",
+        languages: "HTML, CSS",
+        image: ""
+      }, 
+
 ]
 
-function App() {
+function Project() {
     return (
         <div>
-            {groceries.map(grocery => <List grocery={grocery}></List>)}
+            
         </div>
     )
 }
+
+{projects.map(project => <Project project={project}/>)}
