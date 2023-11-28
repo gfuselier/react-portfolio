28a1f3554f6ff867ee606e86b6879984061cf409
gfuselier <gisellefuselier@gmail.com>
2023-11-27 22:07:39 -0600

    added resume link

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,7 +3,7 @@ function Resume() {
     return (
         <div>
             <h2>Resume</h2>
-            <p>Download my resume for more information</p>
+            <p>Download my <a href="https://docs.google.com/document/d/1OJFOlAO_isKn9sAYssI0X9HDYQ8fzNVC2yEiI87bxPM/edit?usp=sharing" download>resume</a> for more information</p>
             <h3>Skills</h3>
             <h4>Front-End</h4>
             <ul className="list">
