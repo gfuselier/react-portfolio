e2639c0aeb87e19207ecf7685237740696f65bca
gfuselier <gisellefuselier@gmail.com>
2023-11-27 20:10:50 -0600

    added base returns

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,18 @@
+
+function Resume() {
+    return (
+        <div>
+            <h2>Resume</h2>
+            <p>Download my resume for more information</p>
+            <h3>Skills</h3>
+            <h4>Front-End</h4>
+            <ul className="list">
+                <li className="list-item">HTML</li>
+                <li className="list-item">CSS</li>
+                <li className="list-item">Javascript</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Resume;
