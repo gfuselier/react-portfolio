b44bb40b371016a2fd64a64dae59d3a2541c45ce
gfuselier <gisellefuselier@gmail.com>
2023-11-30 17:49:03 -0600

    changed link content and path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,19 @@ import { Link } from 'react-router-dom';
 import Navigation from './Navigation';
 
 export default function Header() {
-  // The Nav UI component will render each of the Link elements in the links prop
   return (
     <header className="header">
       <h1>Giselle Fuselier</h1>
       <Navigation
         links={[
           <Link key={1} className="nav-link text-light" to="/">
-            About Me
+            about
           </Link>,
-          <Link key={2} className="nav-link text-light" to="/portfolio">
-            Portfolio
+          <Link key={2} className="nav-link text-light" to="/projects">
+            projects
           </Link>,
-          <Link key={3} className="nav-link text-light" to="/contact">
-            Contact
-          </Link>,
-          <Link key={4} className="nav-link text-light" to="/resume">
-            Resume
+          <Link key={3} className="nav-link text-light" to="/skills">
+            skills
           </Link>
         ]}
       />
