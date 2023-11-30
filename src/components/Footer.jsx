119097352002740d5c63c8d6b397118f920bb642
gfuselier <gisellefuselier@gmail.com>
2023-11-30 17:58:11 -0600

    added email button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,19 @@
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import { FaStackOverflow } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 export default function Footer() {
   return (
     <footer className="text-center">
       <div className="container pt-4">
         <section className="mb-4">
+          <a
+            className="btn btn-link btn-lg m-1"
+            href="mailto:gisellefuselier@gmail.com"
+            type="button"
+          >
+            <MdEmail />
+          </a>
           <a
             className="btn btn-link btn-lg m-1"
             href="https://www.linkedin.com/in/giselle-fuselier-5b9273136/"
@@ -20,13 +27,6 @@ export default function Footer() {
             type="button"
           >
             <FaGithub />
-          </a>
-          <a
-            className="btn btn-link btn-lg m-1"
-            href="https://stackoverflow.com/users/22997318/giselle-fuselier"
-            type="button"
-          >
-            <FaStackOverflow />
           </a>
         </section>
       </div>
