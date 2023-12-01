205895b161d6ba4b0174ae27fb55c8bccf08e0d9
gfuselier <gisellefuselier@gmail.com>
2023-11-30 19:00:22 -0600

    added skills arrays and map

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,18 +1,61 @@
 
-function Resume() {
+const frontEnd = [
+    "HTML/CSS",
+    "JavaScript/jQuery",
+    "Responsive Design",
+    "Bootstrap",
+    "Bulma",
+    "Handlebars",
+    "Progressive Web Applications",
+    "Local Storage, Session Storage, IndexedDB",
+    "React.js"
+]
+
+const backEnd = [
+    "Node.js",
+    "Express.js",
+    "MySQL",
+    "MongoDB",
+    "User Authentication - JWT, Express Sessions",
+    "GraphQL"
+]
+
+const tools = [
+    "Git/Github",
+    "GitHub Pages",
+    "Heroku",
+    "Netlify",
+    "Apollo",
+    "Shell Scripting",
+    "Unit Testing",
+    "Linting"
+]
+
+function Skills() {
     return (
         <div>
-            <h2>Resume</h2>
-            <p>Download my <a href="https://docs.google.com/document/d/1OJFOlAO_isKn9sAYssI0X9HDYQ8fzNVC2yEiI87bxPM/edit?usp=sharing" download>resume</a> for more information</p>
-            <h3>Skills</h3>
+            <h2>Skills</h2>
+            <p>Download my <a href="https://docs.google.com/document/d/1OJFOlAO_isKn9sAYssI0X9HDYQ8fzNVC2yEiI87bxPM/edit?usp=sharing" download>resume</a> for more information.</p>
             <h4>Front-End</h4>
             <ul className="list">
-                <li className="list-item">HTML</li>
-                <li className="list-item">CSS</li>
-                <li className="list-item">Javascript</li>
+                {frontEnd.map((skill) => (
+                    <li className="list-item" key="">{skill}</li>
+                ))}
+            </ul>
+            <h4>Back-End</h4>
+            <ul className="list">
+                {backEnd.map((skill) => (
+                    <li className="list-item" key="">{skill}</li>
+                ))}
+            </ul>
+            <h4>Tools</h4>
+            <ul className="list">
+                {tools.map((skill) => (
+                    <li className="list-item" key="">{skill}</li>
+                ))}
             </ul>
         </div>
     )
 }
 
-export default Resume;
+export default Skills;
